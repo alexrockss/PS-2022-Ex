2a0e3ff74cd313b4973350d092bcfa45c120d075
alexrockss <104759110+alexrockss@users.noreply.github.com>
2022-05-02 20:28:07 +0300

    vm

---
+++ EasyMVVM/MainWindowVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using EasyMVVM;
+
+namespace EasyMVVM
+{
+    internal class MainWindowVM : DependencyObject, INotifyPropertyChanged
+    {
+        public MainWindowVM()
+        {
+            Model m = new();
+            BoundProperty = m.GetData();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private ObservableCollection<string> _BackingProperty;
+
+        public ObservableCollection<string> BoundProperty
+        {
+            get { return _BackingProperty; }
+            set { _BackingProperty = value; PropChanged(nameof(BoundProperty)); }
+        }
+
+        public void PropChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
