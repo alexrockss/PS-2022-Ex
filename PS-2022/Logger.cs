aea2fc92503c53e893739d9d541e326a4ee4c314
alexrockss <104759110+alexrockss@users.noreply.github.com>
2022-05-02 12:49:32 +0300

    ex fix

---
+++ PS-2022/Logger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace UserLogin
+{
+    static class Logger
+    {
+        private static List<string> currentSessionActiviies = new List<string>();
+
+        public static void LogActivity(string activity)
+        {
+            string activityLine = DateTime.Now + ";" + LoginValidation.CurrentUsername + ";" +
+                                  LoginValidation.CurrentUserRole + ";" + activity;
+
+            if (File.Exists("test.txt") == true)
+            {
+                File.AppendAllText("test.txt", activityLine);
+            }
+
+            currentSessionActiviies.Add(activityLine);
+        }
+
+        public static string GetCurrentSessionActivities()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var curr in currentSessionActiviies)
+            {
+                sb.Append(curr);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ PS-2022/UserData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserLogin
+{
+    public static class UserData
+    {
+        private static List<User> testUser;
+
+        public static List<User> TestUser
+        {
+            get
+            {
+                ResetTestUserData();
+                return testUser;
+            }
+            set { }
+        }
+
+        private static void ResetTestUserData()
+        {
+            if (testUser != null) return;
+
+            testUser = new List<User>();
+            testUser.Add(new User("alexandra", "121219092", "", UserRoles.ADMIN, new DateTime(2022, 1, 1)));
+            testUser.Add(new User("alex123", "121219093", "", UserRoles.ADMIN, new DateTime(2022, 2, 2)));
+            testUser.Add(new User("123alex", "121219093", "121219093", UserRoles.STUDENT, new DateTime(2022, 3, 3)));
+        }
+
+        public static void GetUsers()
+        {
+            foreach (var user in testUser)
+            {
+                Console.WriteLine(user.ToString());
+            }
+        }
+
+        public static User IsUserPassCorrect(string username, string password)
+        {
+            foreach (var user in TestUser)
+            {
+                if (user.username == username && user.password == password)
+                    return user;
+            }
+
+            return null;
+        }
+
+        public static void SetUserActiveTo(string username, DateTime activity)
+        {
+            foreach (var user in TestUser)
+            {
+                if (user.username == username)
+                {
+                    user.Created = activity;
+                    Logger.LogActivity("Role changed for " + username);
+                }
+            }
+        }
+
+        public static void AssignUserRole(string username, UserRoles newRole)
+        {
+            foreach (var user in TestUser)
+            {
+                if (user.username == username)
+                {
+                    user.userRole = newRole;
+                    Logger.LogActivity("Activity changed for " + username);
+                }
+            }
+        }
+    }
+}
