91778419851dab104e581675e8c0f78cce263fae
alexrockss <104759110+alexrockss@users.noreply.github.com>
2022-05-09 11:17:39 +0300

    ex 5

--- EasyMVVM/MainWindowVM.cs
+++ EasyMVVM/MainWindowVM.cs
@@ -1,35 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using EasyMVVM;
 
 namespace EasyMVVM
 {
-    internal class MainWindowVM : DependencyObject, INotifyPropertyChanged
+    public class MainWindowVM : DependencyObject, INotifyPropertyChanged
     {
-        public MainWindowVM()
-        {
-            Model m = new();
-            BoundProperty = m.GetData();
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
+        private ObservableCollection<string>? _BackingProperty;
 
-        private ObservableCollection<string> _BackingProperty;
-
-        public ObservableCollection<string> BoundProperty
+        public ObservableCollection<string>? BoundProperty
         {
             get { return _BackingProperty; }
-            set { _BackingProperty = value; PropChanged(nameof(BoundProperty)); }
+            set { _BackingProperty = value; PropChanged("BoundProperty"); }
         }
 
-        public void PropChanged(String propertyName)
+        public MainWindowVM()
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            Model m = new Model();
+            BoundProperty = m.GetData();
         }
+
+        public void PropChanged(string propertyName)
+        {
+            //Did WPF registerd to listen to this event
+            if (PropertyChanged != null)
+            {
+                //Tell WPF that this property changed
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
