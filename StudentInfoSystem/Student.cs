9a176d45897eee60a9faad4de69cc0edaf711132
alexrockss <104759110+alexrockss@users.noreply.github.com>
2022-05-25 16:28:36 +0300

    Add files via upload

---
+++ StudentInfoSystem/Student.cs
@@ -0,0 +1,34 @@
+﻿using StudentInfoSystem.Enums;
+using System;
+
+namespace StudentInfoSystem
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public byte[]? Photo { get; set; }
+
+        public string? Faculty { get; set; }
+
+        public string? Speciality { get; set; }
+
+        public Degree Degree { get; set; }
+
+        public EducationStatus Status { get; set; }
+
+        public string FacultyNumber { get; set; }
+
+        public int Course { get; set; }
+
+        public int Stream { get; set; }
+
+        public int Group { get; set; }
+    }
+}
---
+++ StudentInfoSystem/StudentData.cs
@@ -0,0 +1,39 @@
+﻿using StudentInfoSystem.Enums;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentInfoSystem
+{
+    public class StudentData
+    {
+        public List<Student>? TestStudents { get; private set; }
+
+        public StudentData()
+        {
+            TestStudents = new List<Student>();
+
+            TestStudents?.Add(new Student()
+            {
+                FirstName = "Ivan",
+                MiddleName = "Georgiev",
+                LastName = "Ivanov",
+                Speciality = "Computer and software engineering",
+                Faculty = "FCST",
+                Course = 3,
+                FacultyNumber = "121219",
+                Group = 30,
+                Degree = Degree.Професионално,
+                Status = EducationStatus.Редовен,
+                Stream = 9
+            });
+        }
+
+        public bool IsThereStudent(string facNum)
+        {
+            StudentInfoContext context = new StudentInfoContext();
+
+            Student? result = context.Students.FirstOrDefault(s => s.FacultyNumber == facNum);
+            return result != null;
+        }
+    }
+}
